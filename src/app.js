992c080993ab9016159df7e034d9dfb409e90218
KhanSayeem <khansayeem03@gmail.com>
2025-12-19 04:43:04 +0600

    Remove bcrypt and swapped it for bcryptjs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const path = require("path");
 const { authMiddleware, optionalAuthMiddleware } = require("./auth");
